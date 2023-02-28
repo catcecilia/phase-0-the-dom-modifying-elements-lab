30b274c621ed30596bb3313a6fb614897ba22ffe
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-02-28 18:22:56 -0500

    accidentally added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerText = "Cat is the champion";
+newHeader.innerHTML = "Cat is the champion";
